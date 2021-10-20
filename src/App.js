f67d80a709416bde55b74e301e5e0db1f4c40b40
alejandrojop <alejandro.pacheco@acin.pt>
2021-10-20 12:09:20 +0100

    fetch with poke-api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
 import ReactFCCtest from "react-fcctest";
+import { BasicModal } from "./components/Modal";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=&offset=`)
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results));
+  }, []);
+
   return (
     <div id="quote-box" className="App">
       <ReactFCCtest />
+      <div id="text"></div>
+      <div id="author"></div>
+      <BasicModal data={characters} />
+      <button id="new-quote"></button>
     </div>
   );
 }
